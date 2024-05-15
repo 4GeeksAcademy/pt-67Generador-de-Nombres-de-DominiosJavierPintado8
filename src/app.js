8538e8547f0ca456535a50346c1a306d686c591d
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-05-15 15:39:27 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es", ".net"];
+  let domainNames = [];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < extension.length; e++) {
+          domainNames.push(pronoun[p] + adj[a] + extension[e]);
+        }
+      }
+    }
+  }
+  console.log(domainNames);
 };
